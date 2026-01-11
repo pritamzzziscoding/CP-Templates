b8f7b429ecdd583794dc24974c3e1df9b8c25ff6
Pritam Barman <pritambarmanofficial1608@gmail.com>
2026-01-11 22:46:49 +0530

    just

--- number_theory.cpp
+++ number_theory.cpp
@@ -61,12 +61,13 @@ vector<U> getPrimeFactors(U n){
     return pf;
 }
 
-vector<int> getDivisors(int n){
+template<class T>
+vector<int> getDivisors(T n){
     vector<int> divisors;
     for(int i = 1; i * i <= n; i++){
         if(n % i == 0){
             divisors.push_back(i);
-            if(i * i != n){
+            if(1LL * i * i != n){
                 divisors.push_back(n / i);
             }
         }
